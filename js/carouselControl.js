dd3cc34d7abcf5052b27030246830d866af0c3eb
Alex L <alexlpdj@gmail.com>
2020-12-24 12:31:38 +0100

    finalizado la config del carousel de features, a falta de coppys

--- js/carouselControl.js
+++ js/carouselControl.js
@@ -1,12 +1,26 @@
-
-    $(function () {
-
-        $('.main-carousel').flickity({
-            // options
-            cellAlign: 'left',
-            contain: true,
-            wrapAround: true
-        });
+$(document).ready(function () {
+    console.info('JQUERY CORRECTO');
+    $(".owl-carousel").owlCarousel({
+        loop:true,
+        margin:10,
+        responsiveClass:true,
+        responsive: {
+            0: {
+                items: 1,
+                nav: true
+            },
+            600: {
+                items: 3,
+                nav: false
+            },
+            1000: {
+                items: 1,
+                nav: true,
+                loop: false
+            },
+        }
     });
+});
+
 
 
