1e6091e177be8b67bb440c6129a43b0de6ddbad3
Alex L <alexlpdj@gmail.com>
2021-01-15 14:14:32 +0100

    added some scroll animations

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
-
-function NavbarScrollBehavior(){
+function NavbarScrollBehavior() {
     let prevScrollpos = window.pageYOffset;
-    window.onscroll = function() {
+    window.onscroll = function () {
         let currentScrollPos = window.pageYOffset;
         if (prevScrollpos > currentScrollPos) {
             document.getElementById("navbar").style.top = "0";
@@ -12,6 +11,38 @@ function NavbarScrollBehavior(){
     }
 }
 
-document.addEventListener("DOMContentLoaded", function() {
-   NavbarScrollBehavior();
+function handleStepsRevealOnScroll() {
+    let controller = new ScrollMagic.Controller();
+    let revealElements = document.getElementsByClassName("steps-test");
+    for (let i = 0; i < revealElements.length; i++) { // create a scene for each element
+        new ScrollMagic.Scene({
+            triggerElement: revealElements[i], // y value not modified, so we can use element as trigger as well
+            offset: 50,												 // start a little later
+            triggerHook: 0.9,
+        })
+            .setClassToggle(revealElements[i], "visible") // add class toggle
+            .addTo(controller);
+    }
+}
+
+function handleContactRevealOnScroll() {
+    let controller = new ScrollMagic.Controller();
+    let elementToReveal = document.getElementById("contact");
+
+    new ScrollMagic.Scene({
+        triggerElement: elementToReveal, // y value not modified, so we can use element as trigger as well
+        offset: 100,												 // start a little later
+        triggerHook: 0.9,
+    }).setClassToggle(elementToReveal, "revealed")
+        .addTo(controller);
+
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    NavbarScrollBehavior();
+
+    // SCROLL MAGIC ANIMATIONS
+    handleStepsRevealOnScroll();
+    handleContactRevealOnScroll();
+
 });
