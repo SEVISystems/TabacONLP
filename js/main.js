4e7d2f680e05a8ac6e78a2e03d518006d10cfd6a
Alex L <alexlpdj@gmail.com>
2021-01-15 16:31:31 +0100

    added some scroll animations

--- js/main.js
+++ js/main.js
@@ -11,9 +11,38 @@ function NavbarScrollBehavior() {
     }
 }
 
-function handleStepsRevealOnScroll() {
+function handleHowtoSectionBlocksRevealOnScroll() {
     let controller = new ScrollMagic.Controller();
-    let revealElements = document.getElementsByClassName("steps-test");
+    let revealElements = document.getElementsByClassName("howto-children-block");
+    for (let i = 0; i < revealElements.length; i++) { // create a scene for each element
+        new ScrollMagic.Scene({
+            triggerElement: revealElements[i], // y value not modified, so we can use element as trigger as well
+            offset: i < 3 ? 35*i : 15*i,												 // start a little later
+            triggerHook: 0.9,
+        })
+            .setClassToggle(revealElements[i], "revealed") // add class toggle
+            .addTo(controller);
+    }
+}
+function handleFeaturesSectionBlocksRevealOnScroll() {
+    let controller = new ScrollMagic.Controller();
+    let revealElements = document.getElementsByClassName("features-card");
+    for (let i = 0; i < revealElements.length; i++) { // create a scene for each element
+
+        setTimeout(function(){
+            new ScrollMagic.Scene({
+                triggerElement: revealElements[i], // y value not modified, so we can use element as trigger as well
+                offset: 40,												 // start a little later
+                triggerHook: 0.8,
+            })
+                .setClassToggle(revealElements[i], "revealed") // add class toggle
+                .addTo(controller);
+        }, i*200);
+    }
+}
+function handleInstallationStepsRevealOnScroll() {
+    let controller = new ScrollMagic.Controller();
+    let revealElements = document.getElementsByClassName("step-scroll-revealing");
     for (let i = 0; i < revealElements.length; i++) { // create a scene for each element
         new ScrollMagic.Scene({
             triggerElement: revealElements[i], // y value not modified, so we can use element as trigger as well
@@ -24,14 +53,13 @@ function handleStepsRevealOnScroll() {
             .addTo(controller);
     }
 }
-
 function handleContactRevealOnScroll() {
     let controller = new ScrollMagic.Controller();
     let elementToReveal = document.getElementById("contact");
 
     new ScrollMagic.Scene({
         triggerElement: elementToReveal, // y value not modified, so we can use element as trigger as well
-        offset: 100,												 // start a little later
+        offset: 50,												 // start a little later
         triggerHook: 0.9,
     }).setClassToggle(elementToReveal, "revealed")
         .addTo(controller);
@@ -40,9 +68,10 @@ function handleContactRevealOnScroll() {
 
 document.addEventListener("DOMContentLoaded", function () {
     NavbarScrollBehavior();
-
-    // SCROLL MAGIC ANIMATIONS
-    handleStepsRevealOnScroll();
+    // SCROLL MAGIC
+    handleHowtoSectionBlocksRevealOnScroll();
+    handleFeaturesSectionBlocksRevealOnScroll();
+    handleInstallationStepsRevealOnScroll();
     handleContactRevealOnScroll();
 
 });
