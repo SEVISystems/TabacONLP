cf2db8a5dc72f1a139c926f8ba8ed95590a5a96c
NietakO <al316458@uji.es>
2021-01-18 01:56:16 +0100

    mails solved?

--- js/contact_me.js
+++ js/contact_me.js
@@ -1,39 +1,30 @@
 $(function() {
 
  $('#sendMessageButton').click(function () {
-
-      event.preventDefault();
-
       emailjs.init("user_7dkF06ZityaunBjADBvN3");
-   
-      var name = $("input#name").val();
-      var email = $("input#email").val();
-      var message = $("textarea#message").val();
+
+      var name = $("#name").val();
+      var email = $("#email").val();
+      var message = $("#message").val();
       $("input").removeClass('error');
       $("textarea").removeClass('error');
-
-     if(name !== '' && email !== '' && phone !== '' && message !== ''){
-
-         emailjs.send('service_y3f2hrw', 'template_xbmwatk', {from_mail: email , message_html: message, from_name: name, from_phone: phone});
-
+      console.log("ENVIANDO MAIL")
+     if(name !== '' && email !== '' && message !== ''){
+         emailjs.send('service_y3f2hrw', 'template_xbmwatk', {from_mail: email , message_html: message, from_name: name});
          $('#email').val('');
          $('#message').val('');
          $('#name').val('');
-         $('#phone').val('');
-
-         swal("¡Gracias!", "Nos pondremos en contacto contigo lo más rápido posible", "success");
-
+         alert("MAIL ENVIADO")
      }else{
+         alert("MAIL NO ENVIADO")
          if(name === ''){
-             $("input#name").addClass('error');
-         }if(phone === ''){
-             $("input#phone").addClass('error');
+             $("#name").addClass('error');
+
          }if(email === ''){
-             $("input#email").addClass('error');
+             $("#email").addClass('error');
          }if(message === ''){
-             $("textarea#message").addClass('error');
+             $("#message").addClass('error');
          }
      }
-
  });
 });
