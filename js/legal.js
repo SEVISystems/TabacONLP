afa792479ad5a196a11808e181b42dc3babd6c19
Alex L <alexlpdj@gmail.com>
2021-01-22 16:56:43 +0100

    fixed legal page scrolling links

--- js/legal.js
+++ js/legal.js
@@ -1,4 +1,21 @@
 document.addEventListener("DOMContentLoaded", function () {
 
-        document.getElementById('')
+        let sections = ['law', 'conditions', 'privacy'];
+        let offset = 80;
+
+        for(let i = 0; i < sections.length; i++){
+                let sectionDomElement = document.getElementById(sections[i]);
+                let linkDomElement = document.getElementById(sections[i] + 'Link');
+
+                linkDomElement.addEventListener('click', function(e){
+                        e.preventDefault();
+                        let offsetPosition = sectionDomElement.getBoundingClientRect().top - offset;
+                        window.scrollTo({
+                                top: offsetPosition,
+                                behavior: "smooth"
+                        });
+                }, false);
+
+        }
+
 });
